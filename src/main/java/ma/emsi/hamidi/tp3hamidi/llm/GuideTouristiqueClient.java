85b19406a4644f17097a4b620878b4dc7b5ca9c2
meryemhamidi <meryam@gmail.com>
2025-11-04 11:33:28 +0100

    Creation de la classe GuideTouristiqueClient

---
+++ src/main/java/ma/emsi/hamidi/tp3hamidi/llm/GuideTouristiqueClient.java
@@ -0,0 +1,34 @@
+package ma.emsi.hamidi.tp3hamidi.llm;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class GuideTouristiqueClient implements GuideTouristique {
+
+    private GuideTouristique guide;
+
+    public GuideTouristiqueClient() {
+        String apiKey = System.getenv("GEMINI_KEY");
+        if (apiKey == null || apiKey.isBlank()) {
+            throw new IllegalStateException("Clé API GEMINI_KEY manquante !");
+        }
+
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(apiKey)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.5)
+                .build();
+
+        guide = AiServices.builder(GuideTouristique.class)
+                .chatModel(model)
+                .build();
+    }
+
+    public String ask(String lieu, int nombre) {
+        return guide.ask(lieu, nombre);
+    }
+}
